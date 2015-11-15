0b8951feee8985e54b057fe7220b8c9c2d1d4fb5
lushunming <1357197829@qq.com>
2015-11-15 23:32:20 +0800

    first commit

---
+++ BuilderPatternDemo/src/com/lu/Item.java
@@ -0,0 +1,11 @@
+package com.lu;
+
+public interface Item {
+
+	public String name();
+
+	public Packing packing();
+
+	public float price();
+
+}
---
+++ BuilderPatternDemo/src/com/lu/Packing.java
@@ -0,0 +1,5 @@
+package com.lu;
+
+public interface Packing {
+	public String pack();
+}
