5dbff5460eaa351fbe0e6684fe42a447cea97e7a
lushunming <lushunming>
2015-11-16 14:18:22 +0800

    适配器模式

---
+++ AdapterPattern/src/com/lu/AudioPlayer.java
@@ -0,0 +1,20 @@
+package com.lu;
+
+public class AudioPlayer implements MediaPlayer {
+	private MediaAdapter adapter;
+
+	@Override
+	public void play(String audioType, String fileName) {
+		// 播放 mp3 音乐文件的内置支持
+		if (audioType.equalsIgnoreCase("mp3")) {
+			System.out.println("Playing mp3 file. Name: " + fileName);
+		} // mediaAdapter 提供了播放其他文件格式的支持
+		else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
+			adapter = new MediaAdapter(audioType);
+			adapter.play(audioType, fileName);
+		} else {
+			System.out.println("Invalid media. " + audioType + " format not supported");
+		}
+	}
+
+}
